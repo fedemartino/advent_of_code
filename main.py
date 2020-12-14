dd13c42e11eec4948cbefe8af1070acd70555246
Federico Martino <fedemartino@gmail.com>
2020-12-14 20:40:06 -0300

    change file path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from puzzle4.solution import run as puzzle4
 
 def puzzle1Solution():
     print("-------------- Puzzle 1 -----------")
-    input = extract(".\\puzzle1\input.txt")
+    input = extract("./puzzle1/input.txt")
     print("Part 1 Solution:")
     print(puzzle1(input, 1, 2020))
     print("Part 2 Solution:")
@@ -14,7 +14,7 @@ def puzzle1Solution():
 
 def puzzle2Solution():
     print("-------------- Puzzle 2 -----------")
-    input = extract(".\\puzzle2\input.txt")
+    input = extract("./puzzle2/input.txt")
     print("Part 1 Solution:")
     print(solution_v1(input))
     print("Part 2 Solution:")
@@ -22,7 +22,7 @@ def puzzle2Solution():
 
 def puzzle3Solution():
     print("-------------- Puzzle 3 -----------")
-    input = extract(".\\puzzle3\input.txt")
+    input = extract("./puzzle3/input.txt")
     print("Part 1 Solution:")
     print(puzzle3(input, [[3,1]]))
     print("Part 2 Solution:")
@@ -30,13 +30,13 @@ def puzzle3Solution():
 
 def puzzle4Solution():
     print("-------------- Puzzle 4 -----------")
-    input = extractRaw(".\\puzzle4\input.txt")
+    input = extractRaw("./puzzle4/input.txt")
     print("Part 1 Solution:")
     print(puzzle4(input, ["byr","iyr","eyr","hgt","hcl","ecl","pid"]))
     print("Part 2 Solution:")
     #print(puzzle4(input, [[1,1],[3,1],[5,1],[7,1],[1,2]]))
 
-#puzzle1Solution()
-#puzzle2Solution()
-#puzzle3Solution()
+puzzle1Solution()
+puzzle2Solution()
+puzzle3Solution()
 puzzle4Solution()
