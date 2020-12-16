7c1781a0f4960b750af1877f48940d676b931ce0
Federico Martino <fedemartino@gmail.com>
2020-12-16 07:46:48 -0300

    puzzle 6 solved part 1 and 2

--- puzzle6/solution.py
+++ puzzle6/solution.py
@@ -2,18 +2,20 @@
 
 def run(input):
     groups = input.split("\n\n")
+    #print(groups)
     anyGroupAnswer = 0
     allGroupAnswer = 0
     for group in groups:
         groupAnwers = {}
         personCount = 0
         for person in group.split("\n"):
-            personCount+=1
-            for answer in person:
-                if (answer in groupAnwers.keys()):
-                    groupAnwers[answer] = groupAnwers[answer] + 1
-                else:
-                    groupAnwers[answer] = 1
+            if (person!=""):
+                personCount+=1
+                for answer in person:
+                    if (answer in groupAnwers.keys()):
+                        groupAnwers[answer] = groupAnwers[answer] + 1
+                    else:
+                        groupAnwers[answer] = 1
         for key in groupAnwers.keys():
             if (groupAnwers[key] == personCount):
                 allGroupAnswer+=1
