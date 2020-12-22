ed7eb2dbbcb477e722abeb8284cc5474accd9946
Federico Martino <fedemartino@gmail.com>
2020-12-21 23:20:14 -0300

    puzzle 7 no solution

--- puzzle7/solution.py
+++ puzzle7/solution.py
@@ -1,16 +1,38 @@
 import re
 
-class bag:
-    def __init__(self, bagName, numberBags):
+class Bag:
+    def __init__(self, bagName):
         self.bagName = bagName
+        self.bagList = {}
+    def __str__(self) -> str:
+        return self.bagName + " -> " + str(self.bagList)
+
+    def add(self, bag, bagNumber):
+        self.bagList[bag.bagName] = BagNode(bag, bagNumber)
+
+class BagNode:
+    def __init__(self, bag, numberBags):
+        self.bag = bag
         self.numberBags = numberBags
-        
+    def __str__(self) -> str:
+        return str(self.numberBags) +" : "+  str(self.bag.bagName)
+    
 
 def run(input, bagInput):
-    '''record of which bags can be stored inside the key'''
-    validBagContainers = {}
-    '''record of which bags can contain the key'''
-    reverseValidBagContainers = {}
+    allBags = buildTree(input)
+    for bag in allBags.keys():
+        #pass
+        print(allBags[bag])
+    return len(allBags)
+
+def buildTree(input):
+    allBags = {}
+
+    #'''record of which bags can be stored inside the key'''
+    #validBagContainers = {}
+    #'''record of which bags can contain the key'''
+    #reverseValidBagContainers = {}
+    
     for rule in input:
         '''Split rules in half with container bags and contained bags'''
         splitRule = rule.split("contain")
@@ -18,26 +40,22 @@ def run(input, bagInput):
         rightSideOfRule = splitRule[1]
 
         '''Container bag is everything befer the word "bags" on the left side of the rule'''
-        containerBag = leftSideOfRule[:leftSideOfRule.find(" bags")].strip()
+        containerBag = Bag(leftSideOfRule[:leftSideOfRule.find(" bags")].strip())
+        allBags[containerBag.bagName] = containerBag
 
-        print(containerBag)
+        #print("Container Bag -> "+  str(containerBag))
+        #print(rightSideOfRule)
 
-        
-        print(rightSideOfRule)
         for bag in rightSideOfRule.split(","):
             shortBag = bag[:bag.find("bag")].strip()
             numberPos = re.findall("[0-9].", shortBag)
+            nodeNumber = 0
             if (len(numberPos)):
                 shortBag = shortBag[len(numberPos[0])-1:].strip()
-            print(shortBag)
-
-            if (containerBag in validBagContainers.keys()):
-                validBagContainers[containerBag].append(shortBag)
-            else:
-                validBagContainers[containerBag]= [shortBag]
+                nodeNumber = int(numberPos[0].strip())
             
-            if (shortBag in reverseValidBagContainers.keys()):
-                reverseValidBagContainers[shortBag].append(containerBag)
-            else:
-                reverseValidBagContainers[shortBag] = [containerBag]
-    return len(reverseValidBagContainers[bagInput])
+            containedBag = Bag(shortBag)
+            containerBag.add(containedBag, nodeNumber)
+            #print(bag)
+            #print("Contained Bag -> " + str(containedBag) + " " + str(nodeNumber))
+    return allBags
