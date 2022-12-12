4388540837e8dc73580252f2c8294a931bdca730
Federico Martino <fedemartino@gmail.com>
2022-12-12 17:48:00 -0300

    day 5 start

--- 2022/csharp/src/AdventOfCode/Days/Day5.cs
+++ 2022/csharp/src/AdventOfCode/Days/Day5.cs
@@ -6,6 +6,28 @@ namespace AdventOfCode
     {
         protected override string InternalSolve1(string[] input)
         {
+            List<List<string>> crateList = new List<List<string>>();
+            crateList.Add(new List<string>());
+            foreach (string line in input)
+            {
+                if (line[0..2] == " 1")
+                {
+                    break;
+                }
+                for (int i = 0; i<line.Length;i+=4)
+                {
+                    if (crateList.Count < i/4+1)
+                    {
+                        crateList.Add(new List<string>());
+                    }
+                    string crateLetter = line[(i+1)..(i+2)];
+                    if (crateLetter != " ")
+                    {
+                        crateList[i/4].Add(crateLetter);
+                    }
+                    
+                }
+            }
             return "";
         }
         protected override string InternalSolve2(string[] input)
